67db0fd67acd14d21dc25c5a8ec3824d34da92ae
Hemen Rohani <hemen.rohani@gmail.com>
2022-05-20 08:46:45 +0000

    First commit for created project from Nest CLI

---
+++ README.md
@@ -0,0 +1,3 @@
+# coding-challenge-charging-station-management-system
+
+This repository created for doing has.to.be company coding-challenge.
---
+++ src/app.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller('app')
+export class AppController {
+  constructor(private readonly appService: AppService) { }
+
+  @Post()
+  getHello(): String {
+    return this.appService.getHello();
+  }
+}
